01fac41460a92b0d5c342b31e99f82ddd3a69bb5
huangxin-star <458629403@qq.com>
2023-01-04 11:39:00 +0800

    "test 1"

--- project.R
+++ project.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-customers<-read_delim('Customer Review.csv',delim = ',')
+customers<-read_delim('../Customer Review.csv',delim = ',')
 summary(customers)
 # calculate sd
 sd(customers$ProductPrice)
