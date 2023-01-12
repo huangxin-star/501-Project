17526fe727d28c6cd22b2b5dd461ab3dffd5f317
huangxin-star <458629403@qq.com>
2023-01-12 16:37:50 +0800

    ‘update some clusting code’

--- project.R
+++ project.R
@@ -1,5 +1,13 @@
+install.packages("flexclust")
+install.packages("factoextra")
+install.packages("NbClust")
 library(tidyverse)
-customers<-read_delim('./data/Customer Review.csv',delim = ',')
+library(caret)
+library(cluster)
+library(factoextra)
+library(NbClust)
+library(readxl)
+customers<-read_delim('data/Customer Review.csv',delim = ',')
 summary(customers)
 # calculate sd
 sd(customers$ProductPrice)
@@ -16,6 +24,7 @@ length(unique(customers$UserID))
 length(unique(customers$ProductModelName))
 length(unique(customers$ProductCategory))
 # detect outliers
+install.packages(grid)
 library(gridExtra)
 p1<-ggplot(customers,aes(y=ProductPrice))+geom_boxplot(outlier.colour = "red",outlier.shape = 8,outlier.size = 4)
 p2<-ggplot(customers,aes(y=RetailerZip))+geom_boxplot(outlier.colour = "red",outlier.shape = 8,outlier.size = 4)
@@ -72,3 +81,98 @@ chisq.test(UserGender, correct=F)
 subset_data<-customers[c('UserOccupation','ReviewRating')]
 UserOccupation <- table(subset_data)
 chisq.test(UserOccupation, correct=F)
+
+## Assignment2 PartB
+male <- length(which(customers$UserGender=='Male'))
+female <- length(which(customers$UserGender=='Female'))
+sex <- read_excel("data/sex.xlsx")
+# EDA（这里我会接着补充）
+p <- ggplot(sex,aes((sex),number))
+p+geom_bar(stat = 'identity',aes(fill=sex))
+
+
+# convert category data to numerical data
+# 1. ProductCategory
+productCategory_one <- customers$ProductCategory == 'TV'
+customers[productCategory_one, "ProductCategory.fix"] <- 1
+productCategory_two <- customers$ProductCategory == 'Tablet'
+customers[productCategory_two, "ProductCategory.fix"] <- 2
+productCategory_three <- customers$ProductCategory == 'Smart Phone'
+customers[productCategory_three, "ProductCategory.fix"] <- 3
+productCategory_four <- customers$ProductCategory == 'Laptop'
+customers[productCategory_four, "ProductCategory.fix"] <- 4
+
+# 2. UserGender
+UserGender_one <- customers$UserGender == 'Male'
+customers[UserGender_one, "UserGender.fix"] <- 1
+UserGender_two <- customers$UserGender == 'Female'
+customers[UserGender_two, "UserGender.fix"] <- 2
+
+# 3.UserOccuption
+customers$UserOccupation <- str_to_title(customers$UserOccupation)
+UserOccupation_one <- customers$UserOccupation == 'Manager'
+customers[UserOccupation_one, "UserOccupation.fix"] <- 1
+UserOccupation_two <- customers$UserOccupation == 'Secretary'
+customers[UserOccupation_two, "UserOccupation.fix"] <- 2
+UserOccupation_three <- customers$UserOccupation == 'Programmer'
+customers[UserOccupation_three, "UserOccupation.fix"] <- 3
+UserOccupation_four <- customers$UserOccupation == 'Student'
+customers[UserOccupation_four, "UserOccupation.fix"] <- 4
+UserOccupation_five <- customers$UserOccupation == 'Accountant'
+customers[UserOccupation_five, "UserOccupation.fix"] <- 5
+UserOccupation_six <- customers$UserOccupation == 'Unknown'
+customers[UserOccupation_six, "UserOccupation.fix"] <- 0
+
+# 4.ManufacturerName
+ManufacturerName_one <- customers$ManufacturerName == 'Samsung'
+customers[ManufacturerName_one, "ManufacturerName.fix"] <- 1
+ManufacturerName_two <- customers$ManufacturerName == 'Microsoft'
+customers[ManufacturerName_two, "ManufacturerName.fix"] <- 2
+ManufacturerName_three <- customers$ManufacturerName == 'HP'
+customers[ManufacturerName_three, "ManufacturerName.fix"] <- 3
+ManufacturerName_four <- customers$ManufacturerName == 'Apple'
+customers[ManufacturerName_four, "ManufacturerName.fix"] <- 4
+ManufacturerName_five <- customers$ManufacturerName == 'Dell'
+customers[ManufacturerName_five, "ManufacturerName.fix"] <- 5
+ManufacturerName_six <- customers$ManufacturerName == 'LG'
+customers[ManufacturerName_six, "ManufacturerName.fix"] <- 6
+
+
+# Clusting Test(这里写聚类的代码)
+# 我聚合了一下产品和用户职业
+new <- data_frame(customers$UserOccupation.fix,customers$ProductCategory.fix)
+head(new)
+
+d <- dist(new)  # 计算距离 默认欧式距离
+fit_average <- hclust(d, method="average") # 聚类
+plot(fit_average, hang = -1,  main = "Average Linkage Clustering")
+
+# 围绕中心点的划分（PAM）
+data(new)
+nu_pam <- pam(new, 2, metric="euclidean")
+fviz_cluster(nu_pam, new)
+
+#确定聚类个数
+set.seed(123)
+fviz_nbclust(new, kmeans, method = "wss") +
+  geom_vline(xintercept = 4, linetype = 2)
+
+# 第一种制图方式
+fit.km <- kmeans(new, centers = 4, nstart = 25)
+plot(new, col = fit.km$cluster, pch = 19, frame = FALSE,
+     main = "K-means with k = 4")
+
+# 第二种（好看）
+points(fit.km$centers, col = 1:2, pch = 8, cex = 3)
+fviz_cluster(fit.km, data = new, 
+             ellipse = T, # 增加椭圆
+             ellipse.type = "t", # 椭圆类型
+             geom = "point", # 只显示点不要文字
+             palette = "lancet", # 支持超多配色方案
+             ggtheme = theme_bw() # 支持更换主题
+)
+
+
+
+
+
